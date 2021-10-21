81460f07b46f7a10bc04f3784aecefbd80d4d40b
Molpe <fran.mscr@gmail.com>
2021-10-21 18:52:23 +0200

    chore: changed DB_URL env var

--- configs/db.js
+++ configs/db.js
@@ -2,7 +2,7 @@ const slonik = require("slonik");
 
 const SLONIK_URL =
   process.env.NODE_ENV === "production"
-    ? process.env.DATABASE_URL
+    ? process.env.DB_URL
     : process.env.LOCAL_URL;
 
 const db = slonik.createPool(SLONIK_URL);
